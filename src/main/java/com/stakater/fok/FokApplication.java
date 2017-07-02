19e9c6698dfbf74354b98b8bc026aa04e665b789
Rasheed Amir <rasheed@aurorasolutions.io>
2017-07-02 10:50:10 +0200

    initial spring boot project

---
+++ src/main/java/com/stakater/fok/FokApplication.java
@@ -0,0 +1,12 @@
+package com.stakater.fok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FokApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stakater/fok/FokApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stakater.fok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FokApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
